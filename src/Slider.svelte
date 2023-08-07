6a903ad37b66af827e028e554997b253a53ca52e
unknown <seanpnn00@gmail.com>
2023-08-07 17:48:07 +0200

    color picker modif

---
+++ src/Slider.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let colorName;
+    export let colorValue;
+</script>
+
+<label for={colorName}>{colorName} ({colorValue})</label>
+<input type="range" id={colorName} bind:value={colorValue} min="0" max="255"/>
+
+<style>
+    input[type="range"] {
+        width: 100%;
+    }
+</style>
